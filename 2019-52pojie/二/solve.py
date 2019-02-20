7fc0032b6320a179ebecf8a628f4340ca448f3cb
qianfei11 <490584635@qq.com>
2019-02-20 20:21:41 +0800

    2019/2/20

--- 2019-52pojie/二/solve.py
+++ 2019-52pojie/二/solve.py
@@ -10,11 +10,14 @@ res = ''
 while i != 0:
     c = enc[j]
     if ord(c) < ord(t) or ord(c) > 122:
-        if ord(c) >= 97 and ord(c) <= alpha[num]:
+        if ord(c) >= 97 and ord(c) <= ord(alpha[num]):
             c = chr(ord(c) - num + 26)
+            if ord(c) > ord(alpha[-1]):
+                c = chr(ord(c) - 26)
     else:
         c = chr(ord(c) - num)
     res += c
     j += 1
     i -= 1
 print res
+# HappyNewYear2019
